7599bfe98cf669c931166fc58665f490f81ff8e3
Fionace <xuce1260505@126.com>
2012-09-24 23:21:24 +0800

    Update TcpConn.cpp

--- TcpConn.cpp
+++ TcpConn.cpp
@@ -1,9 +1,15 @@
 #include<TcpConn.h>
 /*构造函数*/
 TcpConn::TcpConn()
-{}
+{
+  sersockfd=-1;
+  Conn=0;
+}
 /*析构函数，要关闭还未关闭的套接字*/
 TcpConn::~TcpConn()
 {
-  if 
+  if (sersockfd != -1)
+      close(sersockfd);
 }
+
+

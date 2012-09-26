abdee3a0eab22413f47dea34e0c6c3d151b11f3f
Fionace <xuce1260505@126.com>
2012-09-26 15:32:53 +0800

    Update TcpConn.cpp

--- TcpConn.cpp
+++ TcpConn.cpp
@@ -12,30 +12,49 @@ TcpConn::~TcpConn()
       close(sersockfd);
 }
 /*初始化服务器监听端口，包括套接字建立，绑定端口，到开始监听的过程*/
-/*有ip和port的情况*/
-bool TcpConn::initConn(const char *ip,int port)
+/*bind仅有port的情况*/
+bool TcpConn::initConn(int port)
 {
   struct sockaddr_in seraddr;
-  struct in_addr=s;
+  //struct in_addr=s;
   seraddr.sin_family=AF_INET;
   seraddr.sin_port=htons(port);
-  inet_aton(ip,&a)
-  seraddr.sin_addr=a;
+  //inet_aton(ip,&a)
+  seraddr.sin_addr.s_addr=htonl(INADDR_ANY);
    if((sersocket=socket(AF_INET,SOCK_STREAM,0))<0)
        {
          printf("Could not create the server socket!\n");
           return -1;
        }
-    if(bind(sersocket,(struct sockaddr)&seraddr,sizeof(serv))!=0)
+    if(bind(sersocket,(struct sockaddr)&seraddr,sizeof(servaddr))!=0)
     {
-      printf("Could not !\n");
+      printf("Could not bind successfully !\n");
           return -1;
     }
+    if(listen(sersocket,SOMAXCONN)!=0)
+    {
+      printf("Could not listen successfully !\n");
+          return -1;
+    }
+    printf("Server begin to listen ...\n");
+    return  1;
 }
-/*仅有port的情况*/
-bool TcpConn::initConn(int port)
+bool TcpConn::AcceptConn()
 {
+  struct sockaddr_in cliaddr;
+  //struct sockaddr cliaddr;
+  int cliaddrLen = sizeof(cliaddr);
+  int cliconn = INVALID_SOCKET;
+  cliconn=accept(sersocket,(sockaddr*)&cliaddr,(socklen_t*)&cliaddrlen);
+  if (cliconn==-1)
+  {
+    printf("Connect to the server failed!\n");
+    return -1
+  }
+  printf("Connect successfully!\n");
+    return  1;
   
+
 }
 
 

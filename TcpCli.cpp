a59e9f414d059e26732f05499320e5608824d44b
Fionace <xuce1260505@126.com>
2012-09-26 16:26:55 +0800

    Update TcpCli.cpp

--- TcpCli.cpp
+++ TcpCli.cpp
@@ -1,12 +1,5 @@
 #include"TcpCli.h"
-Class TcpCli{
-  public:
-   TcpCli();
-   ~TcpCli();
-  bool AttempConn();
-  private:
-  int clisocket;
-};
+
 /*构造函数*/
 TcpCli::TcpCli()
 {
@@ -18,4 +11,34 @@ TcpCli::~TcpCli()
   if (clisocket != -1)
       close(clisocket); 
 }
-bool 
+bool TcpCli::CliSock()
+{
+  clisocket=socket(AF_INET,SOCK_STREAM,0);
+  if(clisocket==-1)
+  {
+    printf("Could not create the client socket!\n");
+    close(clisocket);
+    return -1;
+  }
+   printf("Create the client socket!\n");
+    return  1;
+}
+
+bool TcpCli::AttempConn(const char *ip ,int port)
+{
+  struct sockaddr_in seraddr;
+  bzero(&seraddr,sizeof(seraddr));
+  seraddr.sin_family=AF_INET;
+  seraddr.sin_port=htons(port);
+  inet_pton(AF_INET,ip,&seraddr.sin_addr);
+  if(connect(clisocket,(sockaddr*)&seraddr,sizeof(seraddr))==-1)
+  {
+    printf("Could not connect the server!\n");
+    close(clisocket);
+    return -1;
+  }
+  printf("Client want to connect the server!\n");
+    return  1;
+  
+  
+}
